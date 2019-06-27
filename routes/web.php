a4cdf5ce4d0a3dcc2befe127ee684433384ada4a
Mindexperiment <andrewloyd@libero.it>
2019-06-27 17:25:59 +0200

    open routes for video upload

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// Videos
+Route::post( '/videos/upload', 'VideosController@upload' )->name( 'videos.upload' );
+Route::delete( '/videos/{video}', 'VideosController@destroy' )->name( 'videos.destroy' );
