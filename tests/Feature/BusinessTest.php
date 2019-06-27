16aeed3c6b5bb30723ec2ea73bc6bf5e8e91dfc5
Mindexperiment <andrewloyd@libero.it>
2019-06-27 14:35:18 +0200

    define business test

--- tests/Feature/BusinessTest.php
+++ tests/Feature/BusinessTest.php
@@ -6,8 +6,32 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\Business;
+use App\User;
+
 class BusinessTest extends TestCase {
 
  use RefreshDatabase;
 
+ public function test_AuthUser_CanRegisterABusiness_DatabaseHasBusiness() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+  $business = factory( Business::class )->make();
+
+  $response = $this->post( route( 'business.store' ), $business->toArray() );
+
+  $this->assertDatabaseHas( 'businesses', [ 'vat'=>$business->vat ] );
+  $response->assertRedirect( route( 'business.index' ) );
+ }
+
+ public function test_Business_RegistrationAssignUUID_DatabaseHasFolder() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+  $business = factory( Business::class )->make();
+  $this->post( route( 'business.store' ), $business->toArray() );
+
+  $model = Business::where( 'vat', $business->vat )->first();
+
+  $this->assertDatabaseHas( 'businesses', [ 'id'=>$model->id, 'folder'=>$model->folder ] );
+ }
 }
