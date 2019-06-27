0606abc8ec7c5f4103f29ed353e1cb2938322f72
Mindexperiment <andrewloyd@libero.it>
2019-06-27 12:02:55 +0200

    define user account test

--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -6,8 +6,41 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\User;
+
 class AccountTest extends TestCase {
 
  use RefreshDatabase;
 
+ public function test_User_CanRegisterAnAccount_DatabaseHasUser() {
+  $user = [
+   'name' => 'Name',
+   'email' => 'a@b.c',
+   'password' => 'password',
+   'password_confirmation' => 'password'
+  ];
+
+  $response = $this->post( route( 'register' ), $user );
+
+  $this->assertDatabaseHas( 'users', [ 'email'=>$user['email'] ] );
+ }
+
+ public function test_User_CanLoginIn_IsAuthenticated() {
+  $user = factory( User::class )->create();
+
+  $data = [ 'email'=>$user->email, 'password'=>'password' ];
+  $response = $this->post( route( 'login' ), $data );
+
+  $this->assertAuthenticatedAs( $user );
+ }
+
+ public function test_AuthUser_CanLogOut_IsLoggedOut() {
+  $user = factory( User::class )->create();
+  $data = [ 'email'=>$user->email, 'password'=>'password' ];
+  $this->post( route( 'login' ), $data );
+
+  $response = $this->post( route( 'logout' ) );
+
+  $this->assertGuest();
+ }
 }
