bdfb563fc0e2b27bc4c45e015b89ec31401ff583
Mindexperiment <andrewloyd@libero.it>
2019-06-27 17:54:21 +0200

    open route for video index

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 // Videos
 Route::post( '/videos/upload', 'VideosController@upload' )->name( 'videos.upload' );
 Route::delete( '/videos/{video}', 'VideosController@destroy' )->name( 'videos.destroy' );
+Route::get( '/videos', 'VideosController@index' )->name( 'videos.index' );
