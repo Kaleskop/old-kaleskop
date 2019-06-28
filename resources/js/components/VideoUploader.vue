5c124d595e031ed7702416bc3af8ec15d753ec49
Mindexperiment <andrewloyd@libero.it>
2019-06-28 20:57:04 +0200

    add basic video-uploader component

---
+++ resources/js/components/VideoUploader.vue
@@ -0,0 +1,21 @@
+<template>
+
+<div></div>
+
+</template>
+
+<script>
+import FileUpload from "vue-upload-component";
+
+export default {
+ "name": "video-uploader",
+
+ "components": { FileUpload },
+
+ data() {
+  return {
+   "files": []
+  }
+ }
+}
+</script>
