443e275edfadb57147a33b1a251c2114fc6da8ce
Mindexperiment <andrewloyd@libero.it>
2019-06-28 15:17:52 +0200

    restrict account controller to auth users

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class AccountController extends Controller {
 
  public function __construct() {
+  $this->middleware( 'auth' );
   $this->middleware( 'withBusiness' )->only( 'business' );
  }
 
