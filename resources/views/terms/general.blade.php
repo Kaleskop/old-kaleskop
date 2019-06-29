54b82a02f99ddfeaff8c50e50bd186c3f561fdd3
Mindexperiment <andrewloyd@libero.it>
2019-06-29 13:01:32 +0200

    add general terms page

---
+++ resources/views/terms/general.blade.php
@@ -0,0 +1,70 @@
+@extends( 'layouts.page' )
+
+@section( 'page' )
+<article>
+ <h2>General Terms of Service</h2>
+
+ <p>Welcome to Kaleskop!</p>
+
+ <p>Kaleskop is a registered trademark owned by Worldtimeline di Pretto Andrea Giuseppe (&ldquo;<b>Worldtimeline</b>&ldquo;), an italian company operating under the laws of the European Economic Area, (Rea: VI-329139) located at st. A. Manzoni 6, 36073 Cornedo Vicentino, Italy.</p>
+ <p>These Terms of Service (&ldquo;<b>Terms</b>&ldquo;) apply to your access to and use of the Websites (&ldquo;<b>Websites</b>&ldquo;), Applications (&ldquo;<b>Applications</b>&ldquo;) and other Online Products (&ldquo;<b>Products</b>&ldquo;) and Services (&ldquo;<b>Services</b>&ldquo;) (collectively, our &ldquo;<b>Platform</b>&ldquo;) provided by <b>Worldtimeline</b>.</p>
+
+ <p>Effective, July 1, 2019</p>
+
+ <p>By using our <b>Products</b> and <b>Services</b> or otherwise accessing the <b>Platform</b>, you agree to these <b>Terms</b>.</p>
+ <p>Thanks for using our <b>Products</b> and <b>Services</b>.</p>
+
+ <p>General rules</p>
+
+ <p>Do not misuse our <b>Services</b>. Access them only using the interface and instructions that we provide. We may suspend or stop providing our <b>Services</b> to you if you do not comply with our terms or policies or if we are investigating suspected misconduct.</p>
+ <p>Using our <b>Services</b> does not give you ownership of any intellectual property rights in our <b>Services</b> or the content that you access. This document and the terms do not grant you the right to use any branding or logos used in our <b>Services</b>.</p>
+ <p>In connection with your use of the <b>Services</b>, we may send you service announcements, administrative messages and other information.</p>
+
+ <p>The <b>Terms</b> apply to all users of the <b>Service</b>, including users who are also contributors of Content (&ldquo;<b>Content</b>&ldquo;), on the <b>Service</b>. <b>Content</b> includes the text, software, scripts, graphics, photos, sounds, music, videos, audiovisual combinations, interactive features and other materials you may view on, access through or contribute to the <b>Service</b>.</p>
+
+ <p>Overview of Our Platform</p>
+
+ <p>Our <b>Platform</b> provides a platform that may be used by an organization or group (&ldquo;<b>Business</b>&ldquo;) in order to advertise their brands, products and services (&ldquo;<b>Brands</b>&ldquo;) to a community of interested users (&ldquo;<b>Community</b>&ldquo;).</p>
+ <p>Below is a list of the different roles that may be associated with each user and the main entities involved:</p>
+
+ <dl>
+  <dt>User</dt>
+  <dd>a User is an individual or entity that participates in the <b>Community</b> in some way, such as by acting as an <b>Member</b> or <b>Business</b> or by participating in any other way.</dd>
+
+  <dt>Member</dt>
+  <dd>a Member is an individual or entity that registers a Kaleskop Account (&ldquo;<b>Account</b>&ldquo;) in order to participate in the activities or to share <b>Content</b> through the functions provided by the <b>Platform</b>.</dd>
+
+  <dt>Company</dt>
+  <dd>a Company is a <b>Member</b> that has administrator responsibilities on the <b>Community</b>, including posting and editing its <b>Content</b> and the ability to subscribe to the Kaleskop Advertisement Service (&ldquo;<b>KAS</b>&ldquo;).</dd>
+
+  <dt>Spot</dt>
+  <dd>short-term audiovisual content based on the advertising of a specific business brand, product or service.</dd>
+ </dl>
+
+ <p>Our <b>Platform</b> is integrated with third-party payment services that process any contributions or disbursements. The payment data are not stored, in any way, within the <b>Platform</b>.</p>
+ <p>You acknowledge and agree that: (i) <b>Worldtimeline</b> is not a party to and has no responsibility or liability with respect to any communications, transaction, interactions, disputes, or any relationships whatsoever between users; (ii) <b>Worldtimeline</b> does not have any responsibility to investigate or verify the reputation, conduct, morality or criminal background of any user;</p>
+
+ <p>Privacy e Copyright</p>
+
+ <p><b>Worldtimeline</b> provides a Privacy Policy which explains how we protect your personal information and how we protect your privacy when you use our Services.</p>
+ <p>If you think someone is violating your copyright and you want to inform us, we invite you to submit a formal request of revision of the contents to copyright@kaleskop.com</p>
+
+ <p>Updates of Services</p>
+
+ <p>We are constantly changing and improving our <b>Services</b>. We may add or remove functionalities or features and we may suspend or stop a <b>Service</b> altogether.</p>
+ <p>You can stop using our <b>Services</b> at any time. <b>Worldtimeline</b> may also stop providing <b>Services</b> to you or add or create new limits to our <b>Services</b> at any time.</p>
+
+ <p>Use of Services as a company</p>
+
+ <p>If you are using our <b>Services</b> on behalf of a company, this company accepts all the terms defined in this document.</p>
+ <p>In addition, it will keep <b>Worldtimeline</b> and its affiliates, officers, agents and employees harmless and indemnify them against any claim, cause or action arising out of or relating to the use of the <b>Services</b> or breach of these terms, including any liability or expense resulting from complaints, losses, damages, lawsuits, judgments, litigation costs and lawyers' fees.</p>
+
+ <p>About this document</p>
+
+ <p>Depending on the service requested, this document can be supplemented by a specific document.</p>
+ <p>We may modify these terms or any additional terms that apply to a <b>Service</b>. We may also attempt to notify you by sending an email notification to the address associated with your account or providing notice through our <b>Platform</b>.</p>
+ <p>If you do not agree to the modified terms for a service, you should stop using the service.</p>
+</article>
+
+@endsection
+
