db07253ab0fe3af53661f691b894f1a5d2c553d6
Mindexperiment <andrewloyd@libero.it>
2019-06-30 00:37:30 +0200

    style auth login-form

--- resources/views/auth/login-form.blade.php
+++ resources/views/auth/login-form.blade.php
@@ -1,21 +1,21 @@
 <form method="POST" action="{{ route( 'login' ) }}">
  @csrf
 
- <fieldset>
-  <legend>{{ __( 'Login' ) }}</legend>
+ <fieldset class="mb-8 p-4 border border-transparent">
+  <legend class="p-2 font-light italic">{{ __( 'Login' ) }}</legend>
 
-  <div>
-   <label for="email">{{ __( 'Email address' ) }}</label>
-   <input type="email" name="email" id="email" required />
+  <div class="mb-4">
+   <label for="email" class="block mb-2 font-bold">{{ __( 'Email address' ) }}</label>
+   <input type="email" name="email" id="email" class="appearance-none w-full py-2 px-4 rounded border-2 border-transparent leading-tight" required />
 
    @if ( $errors->has( 'email' ) )
     <p>{{ $errors->first( 'email' ) }}</p>
    @endif
   </div>
 
-  <div>
-   <label for="password">{{ __( 'Password' ) }}</label>
-   <input type="password" name="password" id="password" required />
+  <div class="mb-4">
+   <label for="password" class="block mb-2 font-bold">{{ __( 'Password' ) }}</label>
+   <input type="password" name="password" id="password" class="appearance-none w-full py-2 px-4 rounded border-2 border-transparent leading-tight" required />
 
    @if ( $errors->has( 'password' ) )
     <p>{{ $errors->first( 'password' ) }}</p>
@@ -34,5 +34,5 @@
   </div>
  </fieldset>
 
- <button type="submit">{{ __( 'Login' ) }}</button>
+ <button type="submit" class="select-none cursor-pointer inline-block white-space-no-wrap py-2 px-4 rounded border border-transparent leading-tight text-center">{{ __( 'Login' ) }}</button>
 </form>
