e9f1ea9748ae14a5ec002388a56bef6e0475980d
Mindexperiment <andrewloyd@libero.it>
2019-06-29 13:18:50 +0200

    add terms controller

---
+++ app/Http/Controllers/TermsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TermsController extends Controller {
+
+
+ // - view actions
+
+ public function general() {
+  return view( 'layouts.wrapper', [ 'page'=>'terms.general' ] );
+ }
+
+ public function account() {
+  return view( 'layouts.wrapper', [ 'page'=>'terms.account' ] );
+ }
+
+ public function business() {
+  return view( 'layouts.wrapper', [ 'page'=>'terms.business' ] );
+ }
+
+ public function kas() {
+  return view( 'layouts.wrapper', [ 'page'=>'terms.kas' ] );
+ }
+}
