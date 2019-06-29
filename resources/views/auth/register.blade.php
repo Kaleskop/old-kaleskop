03d3d81addb7be1e976107a255929d8a0f991fef
Mindexperiment <andrewloyd@libero.it>
2019-06-30 00:48:31 +0200

    style auth register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,13 +1,13 @@
 @extends( 'layouts.layout' )
 
 @section( 'body' )
-<section>
- <header>
-  <h2>{{ __( 'New Account' ) }}</h2>
-  <p>{{ config( 'app.name' ) }}</p>
+<section class="sm:w-2/3 md:w-1/2 sm:mx-auto py-16 px-6">
+ <header class="mb-6">
+  <h2 class="mb-2 font-medium text-3xl">{{ __( 'New Account' ) }}</h2>
+  <p class="font-semibold text-2xl">{{ config( 'app.name' ) }}</p>
  </header>
 
- <p>{{ __( 'Already have an account? Please,' ) }} <a href="{{ route( 'login' ) }}">{{ __( 'sign in' ) }}</a></p>
+ <p class="mb-4">{{ __( 'Already have an account? Please,' ) }} <a href="{{ route( 'login' ) }}" class="underline">{{ __( 'sign in' ) }}</a></p>
 
  @include( 'auth.register-form' )
 
