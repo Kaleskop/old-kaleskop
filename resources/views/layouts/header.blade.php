8983730de17a25837b75b08f81d37c7c384d5084
Mindexperiment <andrewloyd@libero.it>
2019-06-30 01:10:31 +0200

    style layouts header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,14 +1,14 @@
-<header>
- <h1><a href="{{ route( 'website.homepage' ) }}">{{ config( 'app.name' ) }}</a></h1>
+<header class="py-2 px-6 flex justify-between items-center">
+ <h1 class="font-semibold text-2xl"><a href="{{ route( 'website.homepage' ) }}">{{ config( 'app.name' ) }}</a></h1>
 
  @guest
-  <a href="{{ route( 'login' ) }}">
+  <a href="{{ route( 'login' ) }}" class="sm:hidden">
    <span></span>
    <span>{{ __( 'Login' ) }}</span>
   </a>
  @endguest
 
- <a href="{{ route( 'website.channels' ) }}">
+ <a href="{{ route( 'website.channels' ) }}" class="sm:hidden">
   <span></span>
   <span>{{ __( 'Channels' ) }}</span>
  </a>
