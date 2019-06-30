8da649f733f4d403ee66ddeb367eb8ee9fef83be
Mindexperiment <andrewloyd@libero.it>
2019-06-30 14:07:23 +0200

    style account verified-email-message

--- resources/views/account/verified-email-message.blade.php
+++ resources/views/account/verified-email-message.blade.php
@@ -1,6 +1,6 @@
 @if ( !Auth::user()->hasVerifiedEmail() )
-<div>
- <p>{{ __( 'We sent you an activation code' ) }}</p>
+<div class="my-4 p-2 border-2 border-transparent font-light">
+ <p class="mb-4 font-medium">{{ __( 'We sent you an activation code' ) }}</p>
  <p>{{ __( 'Check your email and click on the link to verify your account.' ) }}</p>
 </div>
 @endif
