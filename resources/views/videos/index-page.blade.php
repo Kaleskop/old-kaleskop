e1cc4f159a1ab7ea317fa81bfebb017ee3badf5e
Mindexperiment <andrewloyd@libero.it>
2019-06-30 14:44:12 +0200

    style videos index-page

--- resources/views/videos/index-page.blade.php
+++ resources/views/videos/index-page.blade.php
@@ -1,11 +1,11 @@
 @extends( 'layouts.page' )
 
 @section( 'page' )
-<section>
- <header>
-  <h2>{{ __( 'Videos' ) }}</h2>
+<section class="py-16 px-6">
+ <header class="mb-6">
+  <h2 class="mb-2 font-medium text-3xl">{{ __( 'Videos' ) }}</h2>
 
-  <a href="{{ route( 'videos.create' ) }}">{{ __( 'New Video' ) }}</a>
+  <a href="{{ route( 'videos.create' ) }}" class="underline">{{ __( 'New Video' ) }}</a>
  </header>
 
  @include( 'videos.table' )
