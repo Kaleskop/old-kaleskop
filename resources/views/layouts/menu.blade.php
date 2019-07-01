6f53906885d37079bd491c2cd17d1f75e21e4170
Mindexperiment <andrewloyd@libero.it>
2019-07-01 14:59:31 +0200

    style layouts menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,34 +1,34 @@
-<nav>
+<nav class="py-4 flex justify-around sm:flex-col sm:items-center">
 
-<a href="{{ route( 'website.channels' ) }}">
+<a href="{{ route( 'website.channels' ) }}" class="hidden sm:inline-flex sm:mb-8">
   <span></span>
   <span>{{ __( 'Channels' ) }}</span>
  </a>
 
  @guest
- <a href="{{ route( 'login' ) }}">
+ <a href="{{ route( 'login' ) }}" class="hidden sm:inline-flex sm:mb-4">
   <span></span>
   <span>{{ __( 'Login' ) }}</span>
  </a>
  @endguest
 
  @auth
- <a href="{{ route( 'account.index' ) }}">
+ <a href="{{ route( 'account.index' ) }}" class="inline-flex sm:mb-4">
   <span></span>
   <span>{{ __( 'Account' ) }}</span>
  </a>
- <a href="{{ route( 'business.index' ) }}">
+ <a href="{{ route( 'business.index' ) }}" class="inline-flex sm:mb-4">
   <span></span>
   <span>{{ __( 'Business' ) }}</span>
  </a>
  @endauth
 
  @business
- <a href="{{ route( 'videos.index' ) }}">
+ <a href="{{ route( 'videos.index' ) }}" class="inline-flex sm:mb-4">
   <span></span>
   <span>{{ __( 'Videos' ) }}</span>
  </a>
- <a href="{{ route( 'advs.index' ) }}">
+ <a href="{{ route( 'advs.index' ) }}" class="inline-flex sm:mb-4">
   <span></span>
   <span>{{ __( 'Advertisements' ) }}</span>
  </a>
