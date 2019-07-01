f0a840a3330ff3c28bc0991d3f372488623da5ba
Mindexperiment <andrewloyd@libero.it>
2019-07-01 18:11:11 +0200

    test stripe customer

--- tests/Feature/CashierTest.php
+++ tests/Feature/CashierTest.php
@@ -6,8 +6,24 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\Business;
+use App\User;
+use Storage;
+
 class CashierTest extends TestCase {
 
  use RefreshDatabase;
 
+ public function test_AuthUser_BusinessRegistrationCreateStripeCustomer_NotNullStripeId() {
+  Storage::fake( 's3' );
+
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+  $business = factory( Business::class )->make();
+  $this->post( route( 'business.store' ), $business->toArray() );
+
+  $business = Business::where( 'vat', $business->vat )->first();
+
+  $this->assertNotNull( $business->stripe_id );
+ }
 }
