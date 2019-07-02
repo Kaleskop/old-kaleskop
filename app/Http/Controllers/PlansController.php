4eebcb282f9ee1fe2d2d83ff4943c8b101022600
Mindexperiment <andrewloyd@libero.it>
2019-07-02 17:36:22 +0200

    add plans controller

---
+++ app/Http/Controllers/PlansController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+use App\Plan;
+
+class PlansController extends Controller {
+
+ public function store( Request $request ) {
+  $this->validate( $request, [
+   'name'         => [ 'required', 'string', 'max:255', 'unique:plans' ],
+   'description'  => [ 'required', 'string' ],
+   'price'        => [ 'required', 'numeric' ],
+   'product_id'   => [ 'required', 'string' ],
+   'product_name' => [ 'required', 'string' ],
+   'plan_id'      => [ 'required', 'string' ],
+  ] );
+
+  $plan = Plan::create( $request->all() );
+
+  return back();
+ }
+
+ public function update( Request $request, Plan $plan ) {
+  $this->validate( $request, [
+   'name'        => [
+    'required', 'string', 'max:255',
+    Rule::unique( 'plans' )->ignore( $plan->id )
+   ],
+   'description'  => [ 'required', 'string' ],
+   'price'        => [ 'required', 'numeric' ],
+   'product_id'   => [ 'required', 'string' ],
+   'product_name' => [ 'required', 'string' ],
+   'plan_id'      => [ 'required', 'string' ],
+  ] );
+
+  $plan->update( $request->all() );
+
+  return back();
+ }
+
+ public function destroy( Plan $plan ) {
+  $plan->delete();
+
+  return back();
+ }
+}
