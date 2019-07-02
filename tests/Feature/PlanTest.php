c8fa89a66a8f729651b81eaeeb52ace04c00142b
Mindexperiment <andrewloyd@libero.it>
2019-07-02 17:26:50 +0200

    define basic test for plan management

--- tests/Feature/PlanTest.php
+++ tests/Feature/PlanTest.php
@@ -6,8 +6,42 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class AccountTest extends TestCase {
+use App\Plan;
+use App\User;
+
+class PlanTest extends TestCase {
 
  use RefreshDatabase;
 
+ public function test_AuthUser_CanInsertAPlan_HasPlan() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+  $plan = factory( Plan::class )->make();
+
+  $response = $this->post( route( 'plans.store' ), $plan->toArray() );
+
+  $this->assertDatabaseHas( 'plans', [ 'name'=>$plan->name ] );
+ }
+
+ public function test_AuthUser_CanEditAPlan_HasPlan() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+  $plan = factory( Plan::class )->create();
+
+  $data = $plan->toArray();
+  $data['name'] = 'enterprise';
+  $response = $this->patch( route( 'plans.update', $plan ), $data );
+
+  $this->assertDatabaseHas( 'plans', [ 'name'=>$data['name'] ] );
+ }
+
+ public function test_AuthUser_CanDeleteSubscriptionPlan_MissingPlan() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+  $plan = factory( Plan::class )->create();
+
+  $response = $this->delete( route( 'plans.destroy', $plan ) );
+
+  $this->assertDatabaseMissing( 'plans', [ 'id'=>$plan->id ] );
+ }
 }
