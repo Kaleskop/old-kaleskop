80ee2d468daca85e85d34adea966e78d72427104
Mindexperiment <andrewloyd@libero.it>
2019-07-03 18:55:07 +0200

    style advs tile

--- resources/views/advs/tile.blade.php
+++ resources/views/advs/tile.blade.php
@@ -1,9 +1,9 @@
-<article>
- <header>
-  <h3>{{ $adv->title }}</h3>
+<article class="mb-4 p-2 border-2 border-transparent">
+ <header class="mb-4 flex items-center">
+  <h3 class="mr-4 font-medium text-2xl">{{ $adv->title }}</h3>
 
   <publish-button endpoint="{{ route( 'advs.publish', $adv ) }}"{{ $adv->isPublished() ? ' published' : '' }}></publish-button>
  </header>
 
- <p>{{ $adv->endpoint }}</p>
+ <p class="font-light text-sm">{{ $adv->endpoint }}</p>
 </article>
