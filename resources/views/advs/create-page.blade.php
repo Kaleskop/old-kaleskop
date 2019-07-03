a834f228fbb73da694814a117d6883d195e3489b
Mindexperiment <andrewloyd@libero.it>
2019-07-03 18:50:34 +0200

    style advs create-page

--- resources/views/advs/create-page.blade.php
+++ resources/views/advs/create-page.blade.php
@@ -1,16 +1,16 @@
 @extends( 'layouts.page' )
 
 @section( 'page' )
-<section>
- <header>
-  <h2>{{ __( 'New advertisement' ) }}</h2>
+<section class="py-16 px-6">
+ <header class="mb-6">
+  <h2 class="mb-2 font-medium text-3xl">{{ __( 'New advertisement' ) }}</h2>
  </header>
 
  @if ( $videos->isNotEmpty() )
   @include( 'advs.create-form' )
 
   @else
-  <p>{{ __( 'You need to' ) }} <a href="{{ route( 'videos.create' ) }}">{{ __( 'upload a video' ) }}</a> {{ __( 'before continue.' ) }}</p>
+  <p>{{ __( 'You need to' ) }} <a href="{{ route( 'videos.create' ) }}" class="underline">{{ __( 'upload a video' ) }}</a> {{ __( 'before continue.' ) }}</p>
 
  @endif
 
