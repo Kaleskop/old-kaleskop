b459cd2e5c87209065a707bfcdae8c7fa6a47275
Mindexperiment <andrewloyd@libero.it>
2019-07-04 16:30:55 +0200

    style website tile

--- resources/views/website/tile.blade.php
+++ resources/views/website/tile.blade.php
@@ -1,3 +1,3 @@
-<article>
- <h4><a href="{{ route( 'website.advs', $adv ) }}">{{ $adv->title }}</a></h4>
+<article class="mb-6 p-2 w-full hover:shadow-lg">
+ <h4 class="mb-2 font-medium text-xl text-center"><a href="{{ route( 'website.advs', $adv ) }}">{{ $adv->title }}</a></h4>
 </article>
