7d059aed958d6e8ec2937fac75b57b105bd4f84c
Mindexperiment <andrewloyd@libero.it>
2019-07-04 15:29:37 +0200

    add business terms page

---
+++ resources/views/terms/business.blade.php
@@ -0,0 +1,53 @@
+@extends( 'layouts.page' )
+
+@section( 'page' )
+<article>
+ <h2>Terms of Service</h2>
+
+ <p>Effective, July 1, 2019</p>
+
+ <p>This document is an integration to the document <a href="{{ route( 'terms.general' ) }}">General Terms of Service</a>.</p>
+ <p>By using our <b>Products</b> and <b>Services</b> or otherwise accessing the <b>Platform</b>, you agree to these <b>Terms</b>.</p>
+
+ <p>Your Account</p>
+
+ <p>You may need a Kaleskop Business Account (&ldquo;<b>Business Account</b>&ldquo;) in order to use some of our <b>Services</b>. When creating your account you must provide complete and accurate information.</p>
+ <p>You are responsible for the activity that happens on or through your <b>Business Account</b>.</p>
+
+ <p>Content</p>
+
+ <p><b>Content</b> includes the text, software, scripts, graphics, photos, sounds, music, videos, audiovisual combinations, interactive features and other materials you may view on, access through or contribute to the <b>Service</b>.</p>
+ <p><b>Spot</b> includes short-term audiovisual content based on the advertising of a specific business brand, product or service.</p>
+ <p>Your <b>Business Account</b> allow you to upload, submit, store, send or receive content. You retain ownership of any intellectual property rights that you hold in that content but it is necessary to guarantee limited license rights to <b>Worldtimeline</b> and others <b>Users</b>.</p>
+ <p>You acknowledge and agree to be solely responsible for your <b>Content</b> and the consequences of its publication.</p>
+ <p><b>Worldtimeline</b> does not endorse any <b>Content</b> or opinion, recommendation or advice expressed in it and <b>Worldtimeline</b> expressly disclaims any liability with respect to <b>Content</b>.</p>
+ <p><b>Worldtimeline</b> reserves the right to moderate, block or remove <b>Spot</b> that do not conform to the general definition defined in this document.</p>
+ <p>You warrant that you have (and will continue to have during your use of the <b>Service</b>) all necessary licenses, rights, consents, and permissions which are required to enable <b>Worldtimeline</b> to use your <b>Content</b> for the purposes of the provision of the <b>Service</b> and to use your <b>Content</b> in the manner contemplated by the <b>Service</b> and these <b>Terms</b>.</p>
+ <p>You agree that you will not post or upload any <b>Content</b> which contains material which it is unlawful for you to possess in the country in which you are resident, or which it would be unlawful for <b>Worldtimeline</b> to use or possess in connection with the provision of the <b>Service</b>.</p>
+
+ <p>Privacy e Copyright</p>
+
+ <p>When you upload or post <b>Content</b> or <b>Spot</b> to Kaleskop, you grant:</p>
+
+ <ol>
+  <li>to <b>Worldtimeline</b>, a worldwide, non-exclusive, royalty-free, transferable licence (with right to sub-licence) to use, reproduce, distribute, prepare derivative works of, display, and perform that <b>Content</b> in connection with the provision of the <b>Service</b> and otherwise in connection with the provision of the <b>Service</b> and <b>Worldtimeline</b> business, including without limitation for promoting and redistributing part or all of the <b>Service</b> (and derivative works thereof) in any media formats and through any media channels;</li>
+  <li>to each user of the <b>Service</b>, a worldwide, non-exclusive, royalty-free licence to access your <b>Content</b> through the <b>Service</b>, and to use, reproduce, distribute, prepare derivative works of, display and perform such <b>Content</b> to the extent permitted by the functionality of the <b>Service</b> and under these <b>Terms</b>.</li>
+ </ol>
+
+ <p>The above licenses granted by you in <b>Content</b> terminate when you remove or delete your <b>Content</b> from the <b>Platform</b>.</p>
+
+ <p>External links from Kaleskop</p>
+
+ <p>Our <b>Service</b> include hyperlinks to other websites that are not owned or controlled by <b>Worldtimeline</b>. Worldtimeline assumes no responsibility for the content, privacy policies and conditions of use of any third-party website.</p>
+
+ <p>Refunds</p>
+
+ <p>If you are not 100% satisfied with your purchase, within the first seven days, contact our customer support to request a full refund of your most recent subscription.</p>
+
+ <p>About this document</p>
+
+ <p>Depending on the service requested, this document can be supplemented by a specific document.</p>
+ <p>We may modify these terms or any additional terms that apply to a <b>Service</b>. We may also attempt to notify you by sending an email notification to the address associated with your account or providing notice through our <b>Platform</b>.</p>
+</article>
+
+@endsection
