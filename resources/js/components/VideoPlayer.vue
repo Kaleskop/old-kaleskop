c388e1091ec0dffbc17b2399ab0604cb0a834ccb
Mindexperiment <andrewloyd@libero.it>
2019-07-04 11:54:39 +0200

    add player methods

--- resources/js/components/VideoPlayer.vue
+++ resources/js/components/VideoPlayer.vue
@@ -69,6 +69,39 @@ export default {
   return {
    "player": null, "reseted": true
   }
+ },
+
+ mounted() {
+  console.log('mounted..')
+  if (!this.player) { this.init(); }
+ },
+
+ "methods": {
+  init() {
+   console.log('init..');
+   const videoOptions = Object.assign({}, this.globals, this.options);
+
+   const emitState = (event, value) => {
+    if (event) { this.$emit(event, this.player); }
+    if (value) { this.$emit(this.customEvent, { [event]: value }); }
+   }
+
+   this.player = videojs(this.$refs.video, videoOptions);
+   this.player.src(this.getSources());
+   this.player.addClass('vjs-kaleskop');
+   this.player.addClass('vjs-16-9');
+   this.player.addClass('vjs-big-play-centered');
+  },
+
+  getSources() {
+   let sources = [];
+   sources.push({
+    "src": this.adv.video.storage_url,
+    "type": this.adv.video.type
+   });
+
+   return sources;
+  }
  }
 }
 </script>
