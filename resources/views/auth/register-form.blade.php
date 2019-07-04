335a5ad79843d87585b8eb397b8c06697c3134ff
Mindexperiment <andrewloyd@libero.it>
2019-07-04 17:42:08 +0200

    link account terms on auth register-form

--- resources/views/auth/register-form.blade.php
+++ resources/views/auth/register-form.blade.php
@@ -42,7 +42,7 @@
 
   <div>
    <input type="checkbox" name="terms" id="terms" value="true" />
-   <label for="terms">{{ __( 'I agree with the' ) }} <a href="" target="_blank">{{ __( 'Terms of service' ) }}</a></label>
+   <label for="terms">{{ __( 'I agree with the' ) }} <a href="{{ route( 'terms.account' ) }}" target="_blank">{{ __( 'Terms of service' ) }}</a></label>
 
    @if ( $errors->has( 'terms' ) )
     <p>{{ $errors->first( 'terms' ) }}</p>
