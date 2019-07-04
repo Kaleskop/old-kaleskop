bdcc3d8fafe4504c1ee3ce6c7fa5b0f225db32c2
Mindexperiment <andrewloyd@libero.it>
2019-07-04 10:58:05 +0200

    add channels actions for website controller

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -14,4 +14,16 @@ class WebsiteController extends Controller {
  public function homepage() {
   return view( 'website.homepage' );
  }
+
+ public function channels() {
+  $advs = Adv::inRandomOrder()->get();
+
+  return view( 'layouts.wrapper', [ 'page'=>'website.channels-page' ] )
+   ->with( 'advs', $advs );
+ }
+
+ public function advs( Adv $adv ) {
+  return view( 'layouts.wrapper', [ 'page'=>'website.advs-page' ] )
+   ->with( 'adv', $adv );
+ }
 }
