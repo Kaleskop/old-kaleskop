314966edfe6eaac9d903753a0f3aee8115db32f4
Mindexperiment <andrewloyd@libero.it>
2019-07-04 11:51:01 +0200

    add basic video-player component

---
+++ resources/js/components/VideoPlayer.vue
@@ -0,0 +1,29 @@
+<template>
+
+<div>
+ <video class="video-js " ref="video"></video>
+</div>
+
+</template>
+
+<script>
+import _vjs from "video.js";
+const videojs = window.videojs || _vjs;
+
+export default {
+ "name": "video-player",
+
+ "props": {
+  "adv": {
+   "type": Object,
+   "required": true
+  }
+ },
+
+ data() {
+  return {
+   "player": null, "reseted": true
+  }
+ }
+}
+</script>
