781679afd4b86c81b37a5e4e59cb03881ceb4f81
Mindexperiment <andrewloyd@libero.it>
2019-07-07 20:33:22 +0200

    add publish button to advs tile

--- resources/views/advs/tile.blade.php
+++ resources/views/advs/tile.blade.php
@@ -1,5 +1,9 @@
 <article>
- <h3>{{ $adv->title }}</h3>
+ <header>
+  <h3>{{ $adv->title }}</h3>
+
+  <publish-button endpoint="{{ route( 'advs.publish', $adv ) }}"{{ $adv->isPublished() ? ' published' : '' }}></publish-button>
+ </header>
 
  <p>{{ $adv->endpoint }}</p>
 </article>
