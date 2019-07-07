69b5c7b2132cc46b1f3ed8d8fd0fa4b493e5ec47
Mindexperiment <andrewloyd@libero.it>
2019-07-07 18:34:35 +0200

    add basic publish button component

---
+++ resources/js/components/PublishButton.vue
@@ -0,0 +1,27 @@
+<template>
+
+<div></div>
+
+</template>
+
+<script>
+export default {
+ "name": "publish-button",
+
+ "props": {
+  "endpoint": {
+   "type": String,
+   "required": true
+  },
+  "published": {
+   "type": Boolean
+  }
+ },
+
+ data() {
+  return {
+   "isPublished": this.published
+  }
+ }
+}
+</script>
