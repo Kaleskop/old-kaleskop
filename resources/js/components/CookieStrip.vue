8904f84442b448f672c8a5705bdfa337b475a043
Mindexperiment <andrewloyd@libero.it>
2019-07-14 15:35:01 +0200

    add template on vue cookie-strip component

--- resources/js/components/CookieStrip.vue
+++ resources/js/components/CookieStrip.vue
@@ -1,5 +1,12 @@
 <template>
- 
+
+<transition name="fade">
+ <div v-if="isOpen">
+  <p>This website uses cookies to ensure you get the best experience on our website.</p>
+  <button type="button" v-on:click="accept">Got It!</button>
+ </div>
+</transition>
+
 </template>
 
 <script>
