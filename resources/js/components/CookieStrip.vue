eaabc75bb988b84ec5753db89354be087950ad4e
Mindexperiment <andrewloyd@libero.it>
2019-07-14 15:17:24 +0200

    add common methods on cookie-strip component

--- resources/js/components/CookieStrip.vue
+++ resources/js/components/CookieStrip.vue
@@ -4,6 +4,31 @@
 
 <script>
 export default {
- "name": "cookie-strip"
+ "name": "cookie-strip",
+
+ data() {
+  return {
+   "isOpen": false
+  }
+ },
+
+ created() {
+  if ( !this.getVisited() === true ) {
+   this.isOpen = true;
+  }
+ },
+
+ "methods": {
+  getVisited() {
+   return localStorage.getItem('cookie:accepted');
+  },
+  setVisited() {
+   localStorage.setItem('cookie:accepted', true);
+  },
+  accept() {
+   this.setVisited();
+   this.isOpen = false;
+  }
+ }
 }
 </script>
