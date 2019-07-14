2a985bdcf4fc5beac4120459168c703c72724d5f
Mindexperiment <andrewloyd@libero.it>
2019-07-14 17:02:36 +0200

    close #13 - style cookie-stripe component

--- resources/js/components/CookieStrip.vue
+++ resources/js/components/CookieStrip.vue
@@ -1,9 +1,9 @@
 <template>
 
 <transition name="fade">
- <div v-if="isOpen">
-  <p>This website uses cookies to ensure you get the best experience on our website.</p>
-  <button type="button" v-on:click="accept">Got It!</button>
+ <div class="fixed z-50 bottom-0 right-0 w-full md:w-64 md:mb-4 md:mr-4 p-4 shadow-lg md:rounded border-2 border-black text-center bg-white text-black" v-if="isOpen">
+  <p class="mb-4">This website uses cookies to ensure you get the best experience on our website.</p>
+  <button type="button" class="cursor-pointer whitespace-nowrap inline-block py-1 px-2 rounded tracking-wide text-center bg-black text-white" v-on:click="accept">Got It!</button>
  </div>
 </transition>
 
