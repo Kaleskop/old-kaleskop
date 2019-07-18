ad275ca7502c1591b576149a5490cd73d9b6b84d
Mindexperiment <andrewloyd@libero.it>
2019-07-18 18:56:01 +0200

    add global stack for metadata on layouts layout

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,6 +7,9 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}" />
 
+  <!-- Metadata -->
+  @stack( 'metadata' )
+
   <!-- Stylesheets -->
   <link rel="stylesheet" href="{{ asset( 'css/app.css' ) }}" />
   @stack( 'stylesheets' )
