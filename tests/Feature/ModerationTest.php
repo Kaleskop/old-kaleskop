3600c6606a0bb6d3a99a99e5d8845080d2b26c3c
Mindexperiment <andrewloyd@libero.it>
2019-07-24 14:50:22 +0200

    add moderation basic test case

--- tests/Feature/ModerationTest.php
+++ tests/Feature/ModerationTest.php
@@ -6,8 +6,86 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\User;
+use App\Adv;
+use App\Enums\ModStatus;
+
 class ModerationTest extends TestCase {
 
  use RefreshDatabase;
 
+ public function test_Model_CanBecomePending_HasPending() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+
+  $adv = factory( Adv::class )->create();
+  $adv->pend();
+
+  $this->assertDatabaseHas( 'moderations', [
+   'model_id'=>$adv->id,
+   'model_type'=>ADV::class,
+   'status'=>ModStatus::PENDING
+  ] );
+  $this->assertTrue( $adv->isPending() );
+ }
+
+ public function test_Model_CanBecomePostponed_HasPostponed() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+
+  $adv = factory( Adv::class )->create();
+  $adv->postpone();
+
+  $this->assertDatabaseHas( 'moderations', [
+   'model_id'=>$adv->id,
+   'model_type'=>ADV::class,
+   'status'=>ModStatus::POSTPONED
+  ] );
+  $this->assertTrue( $adv->isPostponed() );
+ }
+
+ public function test_Model_CanBecomeRejected_HasRejected() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+
+  $adv = factory( Adv::class )->create();
+  $adv->reject();
+
+  $this->assertDatabaseHas( 'moderations', [
+   'model_id'=>$adv->id,
+   'model_type'=>ADV::class,
+   'status'=>ModStatus::REJECTED
+  ] );
+  $this->assertTrue( $adv->isRejected() );
+ }
+
+ public function test_Model_CanBecomeIllegal_HasIllegal() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+
+  $adv = factory( Adv::class )->create();
+  $adv->illegal();
+
+  $this->assertDatabaseHas( 'moderations', [
+   'model_id'=>$adv->id,
+   'model_type'=>ADV::class,
+   'status'=>ModStatus::ILLEGAL
+  ] );
+  $this->assertTrue( $adv->isIllegal() );
+ }
+
+ public function test_Model_CanBecomeApproved_HasApproved() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+
+  $adv = factory( Adv::class )->create();
+  $adv->approve();
+
+  $this->assertDatabaseHas( 'moderations', [
+   'model_id'=>$adv->id,
+   'model_type'=>ADV::class,
+   'status'=>ModStatus::APPROVED
+  ] );
+  $this->assertTrue( $adv->isApproved() );
+ }
 }
