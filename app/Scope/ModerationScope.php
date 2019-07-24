40ac69c242d144f5250e2496667c96fe47523e11
Mindexperiment <andrewloyd@libero.it>
2019-07-24 15:28:04 +0200

    add moderations scope extensions method

--- app/Scope/ModerationScope.php
+++ app/Scope/ModerationScope.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
+use App\Enums\ModStatus;
+
 class ModerationScope implements Scope {
 
  /**
@@ -35,5 +37,78 @@ class ModerationScope implements Scope {
   foreach( $this->extensions as $extension ) {
    $this->{"add{$extension}"}( $builder );
   }
+ }
+
+
+ // - extensions
+
+ protected function addApproved( Builder $builder ) {
+  $builder->macro( 'approved', function( Builder $builder ) {
+   $model = $builder->getModel();
+   $qualifiedKey = $model->qualifyColumn( $model->getKeyName() );
+
+   $builder->join( 'moderations', 'moderations.model_id', '=', $qualifiedKey )
+    ->where( 'moderations.model_type', '=', get_class( $model ) )
+    ->where( 'moderations.status', '=', ModStatus::APPROVED )
+    ->whereNull( 'moderations.deleted_at' );
+
+   return $builder;
+  } );
+ }
+
+ protected function addPending( Builder $builder ) {
+  $builder->macro( 'pending', function( Builder $builder ) {
+   $model = $builder->getModel();
+   $qualifiedKey = $model->qualifyColumn( $model->getKeyName() );
+
+   $builder->join( 'moderations', 'moderations.model_id', '=', $qualifiedKey )
+    ->where( 'moderations.model_type', '=', get_class( $model ) )
+    ->where( 'moderations.status', '=', ModStatus::PENDING )
+    ->whereNull( 'moderations.deleted_at' );
+
+   return $builder;
+  } );
+ }
+
+ protected function addPostponed( Builder $builder ) {
+  $builder->macro( 'postponed', function( Builder $builder ) {
+   $model = $builder->getModel();
+   $qualifiedKey = $model->qualifyColumn( $model->getKeyName() );
+
+   $builder->join( 'moderations', 'moderations.model_id', '=', $qualifiedKey )
+    ->where( 'moderations.model_type', '=', get_class( $model ) )
+    ->where( 'moderations.status', '=', ModStatus::POSTPONED )
+    ->whereNull( 'moderations.deleted_at' );
+
+   return $builder;
+  } );
+ }
+
+ protected function addRejected( Builder $builder ) {
+  $builder->macro( 'rejected', function( Builder $builder ) {
+   $model = $builder->getModel();
+   $qualifiedKey = $model->qualifyColumn( $model->getKeyName() );
+
+   $builder->join( 'moderations', 'moderations.model_id', '=', $qualifiedKey )
+    ->where( 'moderations.model_type', '=', get_class( $model ) )
+    ->where( 'moderations.status', '=', ModStatus::REJECTED )
+    ->whereNull( 'moderations.deleted_at' );
+
+   return $builder;
+  } );
+ }
+
+ protected function addIllegals( Builder $builder ) {
+  $builder->macro( 'illegals', function( Builder $builder ) {
+   $model = $builder->getModel();
+   $qualifiedKey = $model->qualifyColumn( $model->getKeyName() );
+
+   $builder->join( 'moderations', 'moderations.model_id', '=', $qualifiedKey )
+    ->where( 'moderations.model_type', '=', get_class( $model ) )
+    ->where( 'moderations.status', '=', ModStatus::ILLEGAL )
+    ->whereNull( 'moderations.deleted_at' );
+
+   return $builder;
+  } );
  }
 }
