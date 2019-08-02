b678b92070bb2e28743d7b0493bdc86f08ade190
Mindexperiment <andrewloyd@libero.it>
2019-08-02 14:28:55 +0200

    update database seeder with bouncer roles seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder {
   $this->call( VideoSeeder::class );
   $this->call( AdvSeeder::class );
   $this->call( PlanSeeder::class );
-  $this->call( BouncerSeeder::class );
+  $this->call( BouncerRolesSeeder::class );
  }
 }
