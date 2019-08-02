70588acb52234f0a92075952664477c8c42a1c1f
Mindexperiment <andrewloyd@libero.it>
2019-08-02 16:08:57 +0200

    assign business role to all existing business manager

--- database/seeds/BouncerUsersSeeder.php
+++ database/seeds/BouncerUsersSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 
 use Silber\Bouncer\BouncerFacade as Bouncer;
 use App\User;
+use App\Business;
 
 class BouncerUsersSeeder extends Seeder {
 
@@ -21,6 +22,13 @@ class BouncerUsersSeeder extends Seeder {
   $mod1 = User::where( 'email', '=', 'sil.sar90@gmail.com' )->first();
   if ( $mod1 ) {
    $mod1->assign( 'moderator' );
+  }
+
+  $businesses = Business::all();
+  if ( $businesses->isNotEmpty() ) {
+   foreach( $businesses as $business ) {
+    $business->manager->assign( 'business' );
+   }
   }
  }
 }
