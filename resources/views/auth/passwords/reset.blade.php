9e4ce19d9e9c810e150e6504d1a0fef5cd180117
Mindexperiment <andrewloyd@libero.it>
2019-08-25 23:56:52 +0200

    add link to homepage on password reset

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
  <section class="sm:w-2/3 md:w-1/2 sm:mx-auto py-16 px-6 bg-white">
   <header class="mb-6">
    <h2 class="mb-2 font-medium text-3xl">{{ __( 'Reset password' ) }}</h2>
-   <p class="font-semibold text-2xl">{{ config( 'app.name' ) }}</p>
+   <p class="font-semibold text-2xl"><a href="{{ route( 'website.homepage' ) }}">{{ config( 'app.name' ) }}</a></p>
   </header>
 
   @include( 'auth.passwords.reset-form' )
