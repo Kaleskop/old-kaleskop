d613120064fa6bab51e9059bc757cb6bde3059bc
Mindexperiment <andrewloyd@libero.it>
2019-08-25 23:53:03 +0200

    add background color on password email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,18 +1,20 @@
 @extends(  'layouts.layout' )
 
 @section( 'body' )
-<section class="sm:w-2/3 md:w-1/2 sm:mx-auto py-16 px-6">
- <header class="mb-6">
-  <h2 class="mb-2 font-medium text-3xl">{{ __( 'Reset password' ) }}</h2>
-  <p class="font-semibold text-2xl">{{ config( 'app.name' ) }}</p>
- </header>
+<div class="min-h-screen bg-yellow-600">
+ <section class="sm:w-2/3 md:w-1/2 sm:mx-auto py-16 px-6 bg-white">
+  <header class="mb-6">
+   <h2 class="mb-2 font-medium text-3xl">{{ __( 'Reset password' ) }}</h2>
+   <p class="font-semibold text-2xl">{{ config( 'app.name' ) }}</p>
+  </header>
 
- @if ( session( 'status' ) )
-  <p>{{ session( 'status' ) }}</p>
- @endif
+  @if ( session( 'status' ) )
+   <p>{{ session( 'status' ) }}</p>
+  @endif
 
- @include( 'auth.passwords.email-form' )
+  @include( 'auth.passwords.email-form' )
 
-</section>
+ </section>
+</div>
 
 @endsection
