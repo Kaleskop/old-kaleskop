abaeac6a2aae0f7bd2b4318e28b542f9698f6f67
Mindexperiment <andrewloyd@libero.it>
2019-08-25 23:53:44 +0200

    add background color on password reset

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,14 +1,16 @@
 @extends(  'layouts.layout' )
 
 @section( 'body' )
-<section class="sm:w-2/3 md:w-1/2 sm:mx-auto py-16 px-6">
- <header class="mb-6">
-  <h2 class="mb-2 font-medium text-3xl">{{ __( 'Reset password' ) }}</h2>
-  <p class="font-semibold text-2xl">{{ config( 'app.name' ) }}</p>
- </header>
+<div class="min-h-screen bg-yellow-600">
+ <section class="sm:w-2/3 md:w-1/2 sm:mx-auto py-16 px-6 bg-white">
+  <header class="mb-6">
+   <h2 class="mb-2 font-medium text-3xl">{{ __( 'Reset password' ) }}</h2>
+   <p class="font-semibold text-2xl">{{ config( 'app.name' ) }}</p>
+  </header>
 
- @include( 'auth.passwords.reset-form' )
+  @include( 'auth.passwords.reset-form' )
 
-</section>
+ </section>
+</div>
 
 @endsection
