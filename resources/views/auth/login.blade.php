f741f55ce8155244aa56b369e1dfc336805208d5
Mindexperiment <andrewloyd@libero.it>
2019-08-25 23:48:07 +0200

    add link to homepage on auth login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
  <section class="sm:w-2/3 md:w-1/2 sm:mx-auto py-16 px-6 bg-white">
   <header class="mb-6">
    <h2 class="mb-2 font-medium text-3xl">{{ __( 'Sign in' ) }}</h2>
-   <p class="font-semibold text-2xl">{{ config( 'app.name' ) }}</p>
+   <p class="font-semibold text-2xl"><a href="{{ route( 'website.homepage' ) }}">{{ config( 'app.name' ) }}</a></p>
   </header>
 
   <p class="mb-4">{{ __( "Don't have a account?" ) }} <a href="{{ route( 'register' ) }}" class="underline">{{ __( 'Create One' ) }}</a></p>
