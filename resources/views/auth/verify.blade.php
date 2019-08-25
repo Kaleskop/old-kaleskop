c829d2ce7d18d90cb4c543230844a4ee0a1f1107
Mindexperiment <andrewloyd@libero.it>
2019-08-25 23:48:59 +0200

    add link to homepage on auth verify

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -4,7 +4,7 @@
 <section class="sm:w-2/3 md:w-1/2 sm:mx-auto py-16 px-6">
  <header class=mb-6>
   <h2 class="mb-2 font-medium text-3xl">{{ __( 'Verify Your Email Address' ) }}</h2>
-  <p class="font-semibold text-2xl">{{ config( 'app.name' ) }}</p>
+  <p class="font-semibold text-2xl"><a href="{{ route( 'website.homepage' ) }}">{{ config( 'app.name' ) }}</a></p>
  </header>
 
  @if ( session('resent') )
