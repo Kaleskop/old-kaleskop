c9ea2ce5dafadf43a4876f08a7c4f949a8a170f6
Mindexperiment <andrewloyd@libero.it>
2019-08-31 20:38:48 +0200

    update website advs tile

--- resources/views/website/tile.blade.php
+++ resources/views/website/tile.blade.php
@@ -1,4 +1,8 @@
-<article class="w-full sm:w-1/2 md:w-1/4 mb-6 hover:shadow-lg">
- <img src="{{ $adv->storageUrl }}" alt="Advertisement cover image" />
- <h4 class="mb-2 p-2 font-medium text-xl text-center"><a href="{{ route( 'website.advs', $adv ) }}">{{ $adv->title }}</a></h4>
-</article>
+<div class="w-full sm:w-1/2 md:w-1/4 mb-6 p-2">
+ <a href="{{ route( 'website.advs', $adv ) }}" class="block bg-white">
+  <article class="hover:shadow-lg">
+   <img src="{{ $adv->storageUrl }}" alt="Advertisement cover image" />
+   <h4 class="p-2 font-medium text-xl text-center">{{ $adv->title }}</h4>
+  </article>
+ </a>
+</div>
