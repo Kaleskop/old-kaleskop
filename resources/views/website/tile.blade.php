07452425d99615e70c331d5238f667f2d9532c1a
Mindexperiment <andrewloyd@libero.it>
2019-08-31 17:32:34 +0200

    display cover image on website adv tile

--- resources/views/website/tile.blade.php
+++ resources/views/website/tile.blade.php
@@ -1,3 +1,4 @@
-<article class="mb-6 p-2 w-full hover:shadow-lg">
- <h4 class="mb-2 font-medium text-xl text-center"><a href="{{ route( 'website.advs', $adv ) }}">{{ $adv->title }}</a></h4>
+<article class="w-full sm:w-1/2 md:w-1/4 mb-6 hover:shadow-lg">
+ <img src="{{ $adv->cover_path }}" alt="Advertisement cover image" />
+ <h4 class="mb-2 p-2 font-medium text-xl text-center"><a href="{{ route( 'website.advs', $adv ) }}">{{ $adv->title }}</a></h4>
 </article>
