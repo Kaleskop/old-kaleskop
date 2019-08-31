1b7a26915443a5f2e670a2991a8fcf28ea51cc13
Mindexperiment <andrewloyd@libero.it>
2019-08-31 20:29:20 +0200

    use storage url for cover on website tile

--- resources/views/website/tile.blade.php
+++ resources/views/website/tile.blade.php
@@ -1,4 +1,4 @@
 <article class="w-full sm:w-1/2 md:w-1/4 mb-6 hover:shadow-lg">
- <img src="{{ $adv->cover_path }}" alt="Advertisement cover image" />
+ <img src="{{ $adv->storageUrl }}" alt="Advertisement cover image" />
  <h4 class="mb-2 p-2 font-medium text-xl text-center"><a href="{{ route( 'website.advs', $adv ) }}">{{ $adv->title }}</a></h4>
 </article>
