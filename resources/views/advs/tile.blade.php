e2a1acef538cf45795d67efdca4cd36c1b6298c6
Mindexperiment <andrewloyd@libero.it>
2019-08-31 17:30:10 +0200

    display cover image on advs tile

--- resources/views/advs/tile.blade.php
+++ resources/views/advs/tile.blade.php
@@ -1,9 +1,17 @@
 <article class="mb-4 p-2 border-2 border-black">
- <header class="mb-4 flex items-center">
-  <h3 class="mr-4 font-medium text-2xl">{{ $adv->title }}</h3>
+ <div class="w-full sm:flex">
+  <div class="w-full sm:w-1/4 mr-4">
+   <img src="{{ $adv->cover_path }}" alt="Advertisement cover image" />
+  </div>
 
-  <publish-button endpoint="{{ route( 'advs.publish', $adv ) }}"{{ $adv->isPublished() ? ' published' : '' }}></publish-button>
- </header>
+  <div>
+   <header class="mb-4 flex items-center">
+    <h3 class="mr-4 font-medium text-2xl">{{ $adv->title }}</h3>
 
- <p class="font-light text-sm">{{ $adv->endpoint }}</p>
+    <publish-button endpoint="{{ route( 'advs.publish', $adv ) }}"{{ $adv->isPublished() ? ' published' : '' }}></publish-button>
+   </header>
+
+   <p class="font-light text-sm">{{ $adv->endpoint }}</p>
+  </div>
+ </div>
 </article>
